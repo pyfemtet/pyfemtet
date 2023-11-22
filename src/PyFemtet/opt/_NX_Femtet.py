5f49636e90b9120ed34dd12e3d273419a6622d09
kn514 <148934231+kn514@users.noreply.github.com>
2023-11-22 16:16:29 +0900

    NX_Femtetをexcelなしで動くようにした

--- src/PyFemtet/opt/_NX_Femtet.py
+++ src/PyFemtet/opt/_NX_Femtet.py
@@ -10,20 +10,32 @@ PATH_JOURNAL = os.path.abspath(os.path.join(here, 'update_model_parameter.py'))
 
 
 class NX_Femtet(Femtet):
-    def __init__(self, path_prt, strategy='catch'):
+    def __init__(self, path_prt, strategy=None):
+
+        # 引数の処理
         self.path_prt = os.path.abspath(path_prt)
-        super().setFemtet(strategy) # Femtet がセットされる
+                
+        # Femtet を捕まえる
+        if strategy is None:
+            super().setFemtet()
+        else:
+            super().setFemtet(strategy)
 
     def run(self, df):
         self.update_model_via_NX(df)
         super().run(df)
     
     def update_model_via_NX(self, df):
-        # run_journal を使って prt から x_t を作る / prt と同じ名前の x_t ができる
+        # run_journal を使って prt から x_t を作る
+
+        # Femtet が参照している x_t パスを取得する
+        path_x_t = self.Femtet.Gaudi.LastXTPath
+
         # 先にそれを消しておく
-        path_x_t = os.path.splitext(self.path_prt)[0] + '.x_t'
         if os.path.isfile(path_x_t):
             os.remove(path_x_t)
+
+        # NX Journal を使って x_t を更新する
         exe = r'%UGII_BASE_DIR%\NXBIN\run_journal.exe'
         tmpdict = {}
         for i, row in df.iterrows():
@@ -31,19 +43,16 @@ class NX_Femtet(Femtet):
         strDict = json.dumps(tmpdict)
         env = os.environ.copy()
         subprocess.run(
-            [exe, PATH_JOURNAL, '-args', self.path_prt, strDict],
+            [exe, PATH_JOURNAL, '-args', self.path_prt, strDict, path_x_t],
             env=env,
             shell=True,
             cwd=os.path.dirname(self.path_prt))
+
         # この時点で x_t ファイルがなければモデルエラー
         if not os.path.isfile(path_x_t):
             from .core import ModelError
             raise ModelError
-        # モデルとプロジェクトの更新
-        # self.Femtet.Gaudi.NewModel(False)
-        self.Femtet.Gaudi.Activate()
-        self.Femtet.Gaudi.NewModel(False)
-        self.Femtet.Gaudi.ImportSolidWorks(path_x_t)
+
+        # プロジェクトの更新
         self.Femtet.Gaudi.ReExecute()
-        '''条件等も反映されるという話だったが、残念ながら反映されなかった。'''
 
