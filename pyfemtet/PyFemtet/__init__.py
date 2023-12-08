8ca182536b1976bf2d19835ad3aac0a16c1ff3eb
kn514 <148934231+kn514@users.noreply.github.com>
2023-12-08 15:36:31 +0900

    パッケージ構造を変更

--- pyfemtet/PyFemtet/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._version import __version__
-
-__copyright__    = 'Copyright (C) 2023 Kazuma Naito'
-__license__      = 'GPLv3'
-__author__       = 'Kazuma Naito'
-__author_email__ = 'kazuma.naito@murata.com'
-__url__          = ''
-
-__all__ = ['公開モジュール', ...]
