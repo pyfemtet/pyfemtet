503c4bb0d4409229552dd7d708126c48bc3f59aa
kn514 <148934231+kn514@users.noreply.github.com>
2023-12-29 16:59:00 +0900

    docstring を修正

--- pyfemtet/tools/FemtetClassConst.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:cp932 -*-
-
-class FemtetClassName:
-	CFemtet = "FemtetMacro.Femtet"
-	CGaudiPoint = "FemtetMacro.GaudiPoint"
-	CGaudiVector = "FemtetMacro.GaudiVector"
-	CGaudiPlane = "FemtetMacro.GaudiPlane"
-	CComplex = "FemtetMacro.Complex"
-	CSYZMatrix = "FemtetMacro.CSYZMatrix"
