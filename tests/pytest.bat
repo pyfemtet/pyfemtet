4d3cdfa6dd13b68fafccb6f307e35de7ac6747c5
kn514 <148934231+kn514@users.noreply.github.com>
2024-03-27 09:54:39 +0900

    tests フォルダ内のテストのみ実行するよう pytest を変更

--- tests/pytest.bat
+++ tests/pytest.bat
@@ -1,13 +1,13 @@
 cd %~dp0\..
-poetry run pytest -s -k "not _cad and not _sample"
+poetry run pytest ./tests -s -k "not _cad and not _sample"
 pause
 exit
 
 # all(with stdout)
-poetry run pytest -s
+poetry run pytest ./tests -s
 
 # last failed only
-poetry run pytest -s --lf
+poetry run pytest ./tests -s --lf
 
 # (un)match function only
-poetry run pytest -s -k "not _cad and not _sample"
+poetry run pytest ./tests -s -k "not _cad and not _sample"
