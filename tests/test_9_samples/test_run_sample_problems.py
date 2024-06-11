8e6bce412078e12e2ea2afbeb7d82186a7f71dd7
kn514 <148934231+kn514@users.noreply.github.com>
2024-06-11 09:18:52 +0900

    Add scripts to enhance sample test

--- tests/test_9_samples/test_run_sample_problems.py
+++ tests/test_9_samples/test_run_sample_problems.py
@@ -12,45 +12,54 @@ from win32com.client import Dispatch
 import pyfemtet
 from pyfemtet.dispatch_extensions import _get_hwnds
 
+from test_util import test_util
+
 
 LIBRARY_ROOT = os.path.dirname(pyfemtet.__file__)
-SAMPLE_DIR = os.path.join(LIBRARY_ROOT, 'FemtetPJTSample')
+SAMPLE_DIR = os.path.join(LIBRARY_ROOT, 'opt', 'femprj_sample')
 
-
-def open_femprj(femprj_path):
-    Femtet = Dispatch('FemtetMacro.Femtet')
-    for _ in tqdm(range(5), 'wait for dispatch Femtet'):
-        sleep(1)
-    Femtet.LoadProject(femprj_path, True)
+RECORD_MODE = True
 
 
 def main(py_script_path):
     femprj_path = py_script_path.replace('.py', '.femprj')
 
-    if not os.path.isfile(femprj_path):
-        return None
+    # launch and open Femtet externally
+    test_util.launch_femtet(femprj_path)
 
-    # launch Femtet
-    util.execute_femtet()
-    pid = util.get_last_executed_femtet_process_id()
-    for _ in tqdm(range(5), 'wait for launch Femtet'):
-        sleep(1)
+    try:
 
-    # open femprj
-    p = Process(
-        target=open_femprj,
-        args=(femprj_path,),
-    )
-    p.start()
-    p.join()
+        # run script
+        run([sys.executable, py_script_path], check=True)
 
-    # run script
-    run([sys.executable, py_script_path], check=True)
+        # search generated csv
+        generated_csv_path = test_util.find_latest_csv()
+        if RECORD_MODE:
+            # copy to same directory
+            test_util.record_result(
+                src=generated_csv_path,
+                py_path=py_script_path,
+                suffix='_test_result'
+            )
+        else:
+            # search recorded csv
+            recorded_csv = test_util.py_to_reccsv(
+                py_path=py_script_path,
+                suffix='_test_result'
+            )
+            # raise assertion error if needed
+            test_util.is_equal_result(
+                generated_csv_path,
+                recorded_csv
+            )
 
-    # shutdown Femtet
-    sleep(1)
-    util.close_process(pid)
-    sleep(1)
+        # shutdown Femtet
+        test_util.taskkill_femtet()
+
+    except Exception as e:
+        # shutdown Femtet anyway
+        test_util.taskkill_femtet()
+        raise e
 
 
 def test_sample_gau_ex08_parametric():
@@ -68,17 +77,24 @@ def test_sample_wat_ex14_parametric():
     main(py_script_path)
 
 
+def test_sample_wat_ex14_parametric():
+    py_script_path = os.path.join(SAMPLE_DIR, 'paswat_ex01_parametric.py')
+    main(py_script_path)
+
+
 def test_cad_sample_nx_ex01():
-    py_script_path = os.path.join(SAMPLE_DIR, 'NX_ex01', 'NX_ex01.py')
+    py_script_path = os.path.join(SAMPLE_DIR, 'cad_ex01_NX.py')
     main(py_script_path)
 
 
 def test_cad_sample_sldworks_ex01():
-    py_script_path = os.path.join(SAMPLE_DIR, 'Sldworks_ex01', 'Sldworks_ex01.py')
+    py_script_path = os.path.join(SAMPLE_DIR, 'cad_ex01_SW.py')
     main(py_script_path)
 
 
 if __name__ == '__main__':
     for path in glob(os.path.join(SAMPLE_DIR, '*.py'), recursive=True):
         print(path)
-        main(path)
+        if not 'cad' in path:
+            main(path)
+    # test_sample_gau_ex08_parametric()
