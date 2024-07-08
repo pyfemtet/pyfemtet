bb62fb63660691c90cb5abe3b47897df7e830a8c
kn514 <148934231+kn514@users.noreply.github.com>
2024-07-08 16:36:51 +0900

    Remove unused code

--- pyfemtet/opt/visualization/__init__.py
+++
@@ -1,7 +0,0 @@
-from pyfemtet.opt.visualization._monitor import ResultViewerApp
-from pyfemtet.opt.visualization._monitor import ProcessMonitorApp
-
-__all__ = [
-    'ResultViewerApp',
-    'ProcessMonitorApp',
-]
