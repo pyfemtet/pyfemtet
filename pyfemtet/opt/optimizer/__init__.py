689e59adb3de3c302950b3dcb9c8c6185ae7a3a4
kn514 <148934231+kn514@users.noreply.github.com>
2024-11-14 13:34:25 +0900

    CHANGE: Add PoFBoTorchSampler to pyfemtet.opt.optimizer.

--- pyfemtet/opt/optimizer/__init__.py
+++ pyfemtet/opt/optimizer/__init__.py
@@ -3,10 +3,14 @@ from pyfemtet.opt.optimizer._optuna._optuna import OptunaOptimizer
 from pyfemtet.opt.optimizer._scipy import ScipyOptimizer
 from pyfemtet.opt.optimizer._scipy_scalar import ScipyScalarOptimizer
 
+from pyfemtet.opt.optimizer._optuna._pof_botorch import PoFBoTorchSampler, PoFConfig
+
 __all__ = [
     'ScipyScalarOptimizer',
     'ScipyOptimizer',
     'OptunaOptimizer',
     'AbstractOptimizer',
     'logger',
+    'PoFBoTorchSampler',
+    'PoFConfig',
 ]
