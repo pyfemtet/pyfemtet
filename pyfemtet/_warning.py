3bcd709749df430d3e03a3137d838ccaa3ca6a9c
kn514 <148934231+kn514@users.noreply.github.com>
2024-11-22 09:07:17 +0900

    CHANGE: Add ```changed_feature``` decorator.

--- pyfemtet/_warning.py
+++ pyfemtet/_warning.py
@@ -3,13 +3,39 @@ from functools import wraps
 
 
 def experimental_feature(func):
+
     @wraps(func)
     def wrapper(*args, **kwargs):
-        warnings.warn(f"The function '{func.__name__}' is experimental and may change in the future.", 
-                      category=UserWarning, 
-                      stacklevel=2)
+        warnings.warn(
+            f"The function '{func.__name__}' is experimental and may change in the future.",
+            category=UserWarning,
+            stacklevel=2
+        )
         return func(*args, **kwargs)
+
     return wrapper
+
+
+def changed_feature(version, additional_message=None):
+
+    def changed_feature_wrapper(func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            version_string = f'in version {version}' if version is not None else 'in recent update'
+            additional_message_string = ' ' + additional_message if additional_message is not None else ''
+
+            warnings.warn(
+                f"The behavior of function '{func.__name__}' is changed {version_string}." + additional_message_string,
+                category=UserWarning,
+                stacklevel=2
+            )
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return changed_feature_wrapper
 
 
 if __name__ == '__main__':
@@ -19,5 +45,19 @@ if __name__ == '__main__':
     def my_experimental_function():
         print("This is an experimental function.")
 
+    class Sample:
+        @experimental_feature
+        def add(self, a, b):
+            return a + b
+
     # 実行すると、警告が表示されます。
     my_experimental_function()
+    sample = Sample()
+    print(sample.add(1, 2))
+
+
+    @changed_feature('0.1.0', 'See documentation of `Optimizer` for more details.')
+    def product(a, b):
+        return a * b
+
+    print(product(1, 2))
