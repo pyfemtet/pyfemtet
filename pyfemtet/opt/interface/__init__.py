b252bb6dd3c6be0538be261cdec0a6aa78f18b83
pyfemtet <kazuma.naito@murata.com>
2025-01-20 13:14:52 +0900

    CHANGE: Remove to use _LazyClass

--- pyfemtet/opt/interface/__init__.py
+++ pyfemtet/opt/interface/__init__.py
@@ -1,30 +1,40 @@
 from typing import TYPE_CHECKING
-from pyfemtet._imports import _LazyImport, _lazy_class_factory
+import platform
 
 from pyfemtet.opt.interface._base import FEMInterface
 from pyfemtet.opt.interface._base import NoFEM
 
-if TYPE_CHECKING:
+if (platform.system() == 'Windows') or TYPE_CHECKING:
+    # win32
     from pyfemtet.opt.interface._femtet import FemtetInterface
     from pyfemtet.opt.interface._femtet_with_sldworks import FemtetWithSolidworksInterface
     from pyfemtet.opt.interface._femtet_with_nx import FemtetWithNXInterface
-    from pyfemtet.opt.interface._surrogate import PoFBoTorchInterface
     from pyfemtet.opt.interface._excel_interface import ExcelInterface
 
 else:
-    FemtetInterface = _lazy_class_factory(_LazyImport('pyfemtet.opt.interface._femtet'), 'FemtetInterface')
-    FemtetWithSolidworksInterface = _lazy_class_factory(_LazyImport('pyfemtet.opt.interface._femtet_with_sldworks'), 'FemtetWithSolidworksInterface')
-    FemtetWithNXInterface = _lazy_class_factory(_LazyImport('pyfemtet.opt.interface._femtet_with_nx'), 'FemtetWithNXInterface')
-    PoFBoTorchInterface = _lazy_class_factory(_LazyImport('pyfemtet.opt.interface._surrogate'), 'PoFBoTorchInterface')
-    ExcelInterface = _lazy_class_factory(_LazyImport('pyfemtet.opt.interface._excel_interface'), 'ExcelInterface')
+    class NotAvailableInWindows:
+        def __init__(self, *args, **kwargs):
+            raise RuntimeError('This feature is Windows only.')
 
 
+    class FemtetInterface(NotAvailableInWindows):
+        pass
+    class FemtetWithSolidworksInterface(FemtetInterface):
+        pass
+    class FemtetWithNXInterface(FemtetInterface):
+        pass
+    class ExcelInterface(NotAvailableInWindows):
+        pass
+
+from pyfemtet.opt.interface._surrogate._base import SurrogateModelInterfaceBase
+from pyfemtet.opt.interface._surrogate._singletaskgp import PoFBoTorchInterface
 
 __all__ = [
     'FEMInterface',
     'NoFEM',
     'FemtetInterface',
-    'FemtetWithNXInterface',
     'FemtetWithSolidworksInterface',
+    'FemtetWithNXInterface',
+    'SurrogateModelInterfaceBase',
     'PoFBoTorchInterface',
 ]
