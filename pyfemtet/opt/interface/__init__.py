b6f8e262641d14ab6e29fba517c2ef5e1afc2183
pyfemtet <kazuma.naito@murata.com>
2025-01-20 15:09:29 +0900

    CHANGE: Remove to use _LazyClass

--- pyfemtet/opt/interface/__init__.py
+++ pyfemtet/opt/interface/__init__.py
@@ -1,24 +1,27 @@
+import platform
 from typing import TYPE_CHECKING
-from pyfemtet._imports import _LazyImport, _lazy_class_factory
 
 from pyfemtet.opt.interface._base import FEMInterface
 from pyfemtet.opt.interface._base import NoFEM
 
-if TYPE_CHECKING:
+if (platform.system() == 'Windows') or TYPE_CHECKING:
     from pyfemtet.opt.interface._femtet import FemtetInterface
     from pyfemtet.opt.interface._femtet_with_sldworks import FemtetWithSolidworksInterface
     from pyfemtet.opt.interface._femtet_with_nx import FemtetWithNXInterface
-    from pyfemtet.opt.interface._surrogate import PoFBoTorchInterface
     from pyfemtet.opt.interface._excel_interface import ExcelInterface
 
 else:
-    FemtetInterface = _lazy_class_factory(_LazyImport('pyfemtet.opt.interface._femtet'), 'FemtetInterface')
-    FemtetWithSolidworksInterface = _lazy_class_factory(_LazyImport('pyfemtet.opt.interface._femtet_with_sldworks'), 'FemtetWithSolidworksInterface')
-    FemtetWithNXInterface = _lazy_class_factory(_LazyImport('pyfemtet.opt.interface._femtet_with_nx'), 'FemtetWithNXInterface')
-    PoFBoTorchInterface = _lazy_class_factory(_LazyImport('pyfemtet.opt.interface._surrogate'), 'PoFBoTorchInterface')
-    ExcelInterface = _lazy_class_factory(_LazyImport('pyfemtet.opt.interface._excel_interface'), 'ExcelInterface')
+    class NotAvailableForWindows:
+        def __init__(self, *args, **kwargs):
+            raise NotImplementedError
 
 
+    FemtetInterface = type('FemtetInterface', (NotAvailableForWindows,), {})
+    FemtetWithSolidworksInterface = type('FemtetWithSolidworksInterface', (FemtetInterface,), {})
+    FemtetWithNXInterface = type('FemtetWithNXInterface', (FemtetInterface,), {})
+    ExcelInterface = type('FemtetInterface', (NotAvailableForWindows,), {})
+
+from pyfemtet.opt.interface._surrogate import PoFBoTorchInterface
 
 __all__ = [
     'FEMInterface',
