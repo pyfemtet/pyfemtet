b779ac5a895373be70877e784b5589b9a233fd52
kn514 <148934231+kn514@users.noreply.github.com>
2025-02-20 12:37:50 +0900

    CHANGE: Add `_output_indices` to SurrogateModelInterfaceBase

--- pyfemtet/opt/interface/_surrogate/_base.py
+++ pyfemtet/opt/interface/_surrogate/_base.py
@@ -17,6 +17,7 @@ class SurrogateModelInterfaceBase(FEMInterface, ABC):
             self,
             history_path: str = None,
             train_history: History = None,
+            _output_indices: dict[int, str] = None,
     ):
 
         self.train_history: History
@@ -25,6 +26,7 @@ class SurrogateModelInterfaceBase(FEMInterface, ABC):
         self.obj: dict[str, float] = dict()
         self.df_prm: pd.DataFrame
         self.df_obj: pd.DataFrame
+        self._output_indices = _output_indices
 
         # history_path が与えられた場合、train_history をコンストラクトする
         if history_path is not None:
@@ -54,10 +56,31 @@ class SurrogateModelInterfaceBase(FEMInterface, ABC):
         self.df_prm = df_prm
         self.df_obj = df_obj
 
+        # self._output_indices が与えられている場合、
+        # history から objective の設定を読み込む
+        if self._output_indices is not None:
+            _load_problem_from_me: bool = True
+
         FEMInterface.__init__(
             self,
             train_history=train_history,  # コンストラクト済み train_history を渡せば並列計算時も何もしなくてよい
         )
+
+
+    def load_objective(self, opt) -> None:
+        from pyfemtet.opt._femopt_core import Objective
+
+        assert self._output_indices is not None
+
+        for index, direction in self._output_indices:
+            obj_name = self.train_history.obj_names[index]
+            opt.objectives[obj_name] = Objective(
+                lambda obj_name_=obj_name: self.obj[obj_name_],
+                name=obj_name,
+                direction=direction,
+                args=(),
+                kwargs={},
+            )
 
     def filter_feasible(self, x: np.ndarray, y: np.ndarray, return_feasibility=False):
         feasible_idx = np.where(~np.isnan(y.sum(axis=1)))
