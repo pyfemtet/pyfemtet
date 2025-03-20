272d48ffd4aef66807b5b4a1af21d7470321a57b
kn514 <148934231+kn514@users.noreply.github.com>
2025-03-20 09:29:39 +0900

    FIX: Remove unused code.

--- v1/visualization2/_process_monitor/application.py
+++ v1/visualization2/_process_monitor/application.py
@@ -4,14 +4,14 @@ from threading import Thread
 
 from flask import jsonify
 
-# from pyfemtet.opt.visualization._base import PyFemtetApplicationBase, logger
-# from pyfemtet.opt.visualization._process_monitor.pages import HomePage, WorkerPage, PredictionModelPage, OptunaVisualizerPage
-# from pyfemtet._message import Msg
-
 from v1.visualization2._base import PyFemtetApplicationBase
 from v1.visualization2._process_monitor.pages import HomePage, WorkerPage, PredictionModelPage, OptunaVisualizerPage
 from v1.worker_status import WorkerStatus
 from pyfemtet._message import Msg
+
+from v1.logger import get_module_logger
+
+logger = get_module_logger('opt.femopt', False)
 
 
 class ProcessMonitorApplication(PyFemtetApplicationBase):
@@ -53,8 +53,6 @@ class ProcessMonitorApplication(PyFemtetApplicationBase):
 
         self.is_debug = is_debug
 
-        # type hint (avoid circular import)
-
         # register arguments
         self.local_data = history.get_df()  # scheduler への負荷を避けるためアクセスは while loop の中で行う
         self.worker_addresses: List[str] = worker_addresses
@@ -64,8 +62,6 @@ class ProcessMonitorApplication(PyFemtetApplicationBase):
     def setup_callback(self, debug=False):
         if not debug:
             super().setup_callback()
-
-        from pyfemtet.opt._femopt_core import OptimizationStatus
 
         @self.server.route('/interrupt')
         def some_command():
@@ -103,10 +99,12 @@ class ProcessMonitorApplication(PyFemtetApplicationBase):
 
             # terminate_all 指令があれば flask server をホストするプロセスごと終了する
             if self.entire_status.value >= self.entire_status.terminated:
-                return 0  # take server down with me
+                break
 
             # interval
             sleep(1)
+
+        return 0  # take server down with me
 
     @staticmethod
     def get_status_color(status: WorkerStatus):
@@ -130,50 +128,6 @@ class ProcessMonitorApplication(PyFemtetApplicationBase):
         return color
 
 
-def g_debug():
-    import os
-    os.chdir(os.path.dirname(__file__))
-
-    from pyfemtet.opt._femopt_core import History, OptimizationStatus
-
-    class _OS(OptimizationStatus):
-
-        # noinspection PyMissingConstructor
-        def __init__(self, name):
-            self.name = name
-            self.st = self.INITIALIZING
-
-        def set(self, status_const):
-            self.st = status_const
-
-        def get(self) -> int:
-            return self.st
-
-        def get_text(self) -> int:
-            return self.const_to_str(self.st)
-
-    g_application = ProcessMonitorApplication(
-        history=History(history_path=os.path.join(os.path.dirname(__file__), '..', 'result_viewer', 'sample', 'sample.csv')),
-        status=_OS('entire'),
-        worker_addresses=['worker1', 'worker2', 'worker3'],
-        worker_status_list=[_OS('worker1'), _OS('worker2'), _OS('worker3')],
-        is_debug=False,
-    )
-
-    g_home_page = HomePage(Msg.PAGE_TITLE_PROGRESS)
-    g_rsm_page = PredictionModelPage(Msg.PAGE_TITLE_PREDICTION_MODEL, '/prediction-model', g_application)
-    g_optuna = OptunaVisualizerPage(Msg.PAGE_TITLE_OPTUNA_VISUALIZATION, '/optuna', g_application)
-    g_worker_page = WorkerPage(Msg.PAGE_TITLE_WORKERS, '/workers', g_application)
-
-    g_application.add_page(g_home_page, 0)
-    g_application.add_page(g_rsm_page, 1)
-    g_application.add_page(g_optuna, 2)
-    g_application.add_page(g_worker_page, 3)
-    g_application.setup_callback(debug=False)
-
-    g_application.run(debug=False)
-
-
 def main(history, status, worker_addresses, worker_status_list, host=None, port=None, host_record=None):
     g_application = ProcessMonitorApplication(history, status, worker_addresses, worker_status_list)
 
@@ -190,6 +144,4 @@ def main(history, status, worker_addresses, worker_status_list, host=None, port=
 
     g_application.start_server(host, port, host_record)
 
-
-if __name__ == '__main__':
-    g_debug()
+    logger.info('Monitor terminated gracefully.')
