271f49057c6d8fa409ac96dc707d6f3017dfb175
kn514 <148934231+kn514@users.noreply.github.com>
2025-03-21 14:57:07 +0900

    CHANGE: Windows 限定機能を分離

--- pyfemtet/opt/interface/__init__.py
+++ pyfemtet/opt/interface/__init__.py
@@ -1,2 +1,6 @@
+import platform
+
 from pyfemtet.opt.interface.interface import AbstractFEMInterface, NoFEM
-from pyfemtet.opt.interface.femtet_interface.femtet_interface import FemtetInterface
+
+if platform.system() == 'Windows':
+    from pyfemtet.opt.interface.femtet_interface.femtet_interface import FemtetInterface
