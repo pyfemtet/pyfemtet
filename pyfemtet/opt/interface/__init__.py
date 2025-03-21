b7c9982e06ef9b2304522f096ea6dd8c305f288c
kn514 <148934231+kn514@users.noreply.github.com>
2025-03-21 15:00:03 +0900

    CHANGE: モジュール構造を変更

--- pyfemtet/opt/interface/__init__.py
+++ pyfemtet/opt/interface/__init__.py
@@ -1,6 +1,6 @@
 import platform
 
-from pyfemtet.opt.interface.interface import AbstractFEMInterface, NoFEM
+from .interface import AbstractFEMInterface, NoFEM
 
 if platform.system() == 'Windows':
-    from pyfemtet.opt.interface.femtet_interface.femtet_interface import FemtetInterface
+    from .femtet_interface import FemtetInterface
