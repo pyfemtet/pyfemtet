612f943be62ab87aa1c2138d5e7e3b79ab05cef0
kn514 <148934231+kn514@users.noreply.github.com>
2025-03-28 13:19:09 +0900

    CHANGE: `_WithExcelSettingsInterface` の型ヒントを修正・Femtet パラメトリック結果出力機能を使う機能の追加、Interface のインポート構成を変更

--- pyfemtet/opt/interface/__init__.py
+++ pyfemtet/opt/interface/__init__.py
@@ -4,3 +4,10 @@ from .interface import AbstractFEMInterface, NoFEM
 
 if platform.system() == 'Windows':
     from .femtet_interface import FemtetInterface
+    from .femtet_with_nx_interface import FemtetWithNXInterface
+    from .femtet_with_solidworks import FemtetWithSolidworksInterface
+    from .excel_interface import ExcelInterface
+    from .with_excel_settings import *
+
+from .surrogate_model_interface import BoTorchInterface
+from .surrogate_model_interface import PoFBoTorchInterface
