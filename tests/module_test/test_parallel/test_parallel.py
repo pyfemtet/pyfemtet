cf0e259782ca3b00b1e09485e0c904299db13a57
kn <148934231+pyfemtet@users.noreply.github.com>
2025-10-02 16:34:59 +0900

    TEST: リファクタリング

--- tests/module_test/test_parallel/test_parallel.py
+++ tests/module_test/test_parallel/test_parallel.py
@@ -1,9 +1,10 @@
+import os
+from time import sleep
+from optuna.samplers import RandomSampler
 from pyfemtet.opt import FEMOpt
 from pyfemtet.opt.interface import NoFEM, FemtetInterface
 from pyfemtet.opt.optimizer import OptunaOptimizer
-from optuna.samplers import RandomSampler
-from time import sleep
-import os
+from tests.utils.reccsv_processor import RECCSV
 
 
 here = os.path.dirname(__file__)
@@ -18,17 +19,7 @@ def obj(_, opt: OptunaOptimizer):
 def test_parallel_nofem(record=False):
 
     base_path = os.path.join(here, 'test_parallel_record')
-
-    csv_path = base_path + '.csv'
-    db_path = base_path + '.db'
-    reccsv_path = base_path + '.reccsv'
-    if record:
-        if os.path.isfile(reccsv_path):
-            os.remove(reccsv_path)
-    if os.path.isfile(csv_path):
-        os.remove(csv_path)
-    if os.path.isfile(db_path):
-        os.remove(db_path)
+    reccsv = RECCSV(base_path, record)
 
     os.environ['DEBUG_FEMOPT_PARALLEL'] = '1'
 
@@ -41,33 +32,17 @@ def test_parallel_nofem(record=False):
     femopt.add_parameter('x2', 1, 0, 1)
     femopt.add_objective('y', obj, args=(opt,))
     femopt.set_random_seed(42)
-    dif_df = femopt.optimize(
+    df = femopt.optimize(
         with_monitor=False,
         confirm_before_exit=False,
         n_parallel=3,
         n_trials=6,
-        history_path=csv_path,
+        history_path=reccsv.csv_path,
     )
 
     check_columns_float = ['x1']
     check_columns_str = ['messages']
-    if record:
-        os.rename(csv_path, reccsv_path)
-    else:
-        from pyfemtet.opt.history import History
-        reference = History()
-        reference.load_csv(reccsv_path, with_finalize=True)
-        ref_df = reference.get_df()
-        for col in check_columns_float:
-            ref = [f'{v: .4e}' for v in ref_df[col].values]
-            dif = [f'{v: .4e}' for v in dif_df[col].values]
-            is_ok = [r == d for r, d in zip(ref, dif)]
-            assert all(is_ok), f'{col} error.'
-        for col in check_columns_str:
-            ref = ref_df[col].values
-            dif = dif_df[col].values
-            is_ok = [r == d for r, d in zip(ref, dif)]
-            assert all(is_ok), f'{col} error.'
+    reccsv.check(check_columns_float, check_columns_str, df)
 
 
 if __name__ == '__main__':
